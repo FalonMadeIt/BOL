429387d78e98755d3a22dbee947d608776013471
Falon Thomas <falont23@gmail.com>
2016-08-13 17:55:19 -0400

    1st host push

--- gulpfile.js
+++ gulpfile.js
@@ -10,5 +10,5 @@ gulp.task('serve', function() {
     }
   });
 
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js', '*.svg'], {cwd: 'app'}, reload);
 });
